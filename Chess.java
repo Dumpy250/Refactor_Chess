4efc8d115f1bfed1c39a60e9003ba5875c92ae67
Dumpy <cameron250taylor@outlook.com>
2024-01-26 12:10:42 -0500

    refactor

--- Chess.java
+++ Chess.java
@@ -2,75 +2,79 @@
 
 import java.util.Scanner;
 
-
-
 public class Chess {
+
+    public static String [][] Board = new String[8][8];
+
+    public static String[][] initializeBoard() {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            Board[i][j] = " ";
+        }
+    }
+            // Rooks
+            Board [0][0] = "R1";
+            Board [0][7] = "R2";
+            Board [7][0] = "R3";
+            Board [7][7] = "R4";
+            
+            // Knights
+            Board [0][1] = "N1";
+            Board [0][6] = "N2";
+            Board [7][1] = "N3";
+            Board [7][6] = "N4";
+    
+            //Bishops
+            Board [0][2] = "B1";
+            Board [0][5] = "B2";
+            Board [7][2] = "B3";
+            Board [7][5] = "B4";
+    
+            //Queens
+            Board [0][3] = "Q1";
+            Board [7][3] = "Q2";
+    
+            //Kings
+            Board [0][4] = "K1";
+            Board [7][4] = "K2";
+    
+        //Pawns
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 1; j++) {
+                 Board[1][i] = "P" + (i + 1);
+                Board[6][i] = "P" + (i + 1);
+            }
+         }
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(Board[i][j]);
+            }
+                System.out.println();
+             }
+    return Board;
+}
 
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 		int i;
 		int j;
 		String PlayerPiece;
-		int GridB;
-		int GridA;
-		String [][] Grid = new String[8][8];
-	    for (i = 0; i < 8; i++) {
-            for (j = 0; j < 8; j++) {
-                Grid[i][j] = " ";
-            }
-        }
-        // Rooks
-        Grid [0][0] = "R1";
-        Grid [0][7] = "R2";
-        Grid [7][0] = "R3";
-        Grid [7][7] = "R4";
-        
-        // Knights
-        Grid [0][1] = "N1";
-        Grid [0][6] = "N2";
-        Grid [7][1] = "N3";
-        Grid [7][6] = "N4";
+		int BoardB;
+		int BoardA;
 
-        //Bishops
-        Grid [0][2] = "B1";
-        Grid [0][5] = "B2";
-        Grid [7][2] = "B3";
-        Grid [7][5] = "B4";
-
-        //Queens
-        Grid [0][3] = "Q1";
-        Grid [7][3] = "Q2";
-
-        //Kings
-        Grid [0][4] = "K1";
-        Grid [7][4] = "K2";
-
-        //Pawns
-        for (i = 0; i < 8; i++) {
-        	for (j = 0; j < 1; j++) {
-        		Grid[1][i] = "P" + (i + 1);
-        		Grid[6][i] = "P" + (i + 1);
-        	}
-        }
-        for (i = 0; i < 8; i++) {
-            for (j = 0; j < 8; j++) {
-                 System.out.print(Grid[i][j]);
-             }
-             System.out.println();
-         }
-        
+        initializeBoard();
         
         	System.out.println("Pick the Piece you want to move");
         	PlayerPiece = scnr.next(); 
         	System.out.println("Enter the Grid you would like to move to, starting with only the row");
-        	GridA = scnr.nextInt();
+        	BoardA = scnr.nextInt();
         	System.out.println("Enter the Column of your move");
-        	GridB = scnr.nextInt();
-        	Grid[GridA][GridB] = PlayerPiece;
+        	BoardB = scnr.nextInt();
+        	Board[BoardA][BoardB] = PlayerPiece;
         	
              for (i = 0; i < 8; i++) {
                  for (j = 0; j < 8; j++) {
-                      System.out.print(Grid[i][j]);
+                      System.out.print(Board[i][j]);
                   }
                   System.out.println();
               }
